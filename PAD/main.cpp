5bc56970d8e7d6409f4a01950b707bb876efcac7
Aaron Vizzini <aaronvizzini@gmail.com>
2016-03-20 18:13:20 +0100

    Initial Commit

---
+++ PAD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PAD
+//
+//  Created by Aaron Vizzini on 20/03/2016.
+//  Copyright © 2016 ESIGELEC-IS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
