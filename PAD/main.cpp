95492d68797dfdb1d37516d04abd155558c2ebc8
Aaron Vizzini <aaronvizzini@gmail.com>
2016-03-28 16:37:52 +0200

    Made the link between the software and the DB, now all of the ROIs will be persistent. Also, added ability to delete a ROI as well as logging of parking information to the DB.

--- PAD/main.cpp
+++ PAD/main.cpp
@@ -12,10 +12,17 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "EdgeDetect.hpp"
-#include "ROIUtils.hpp"
+#include "DBManager.hpp"
 
 using namespace std;
 using namespace cv;
+
+const int CAMERA_ID = 1;
+const int PARKING_LOT_ID = 1;
+const int FRAME_DELAY = 1000;
+const int LOG_TIME = 1000*60*5;//5 mins
+
+DBManager dbm;
 
 Mat videoFrame; //The current video frame being looked at
 Mat roisOverlay; //The overlap which draws the colored polygons for the rois
@@ -25,6 +32,7 @@ int clicks = 0; //Keeps track of mouse clicks to know what point is being assign
 Point one, two, three, four; //The points being given by the user to build the rois
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
+    //Create ROI
     if( event == EVENT_LBUTTONDOWN ) {
         clicks++;
         
@@ -37,27 +45,54 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
         } else if(clicks == 4){
             four = Point(x,y);
             
-            roi test;
-            test.a = one;
-            test.b = two;
-            test.c = three;
-            test.d = four;
-            rois.push_back(test);
+            roi insert;
+            insert.a = one;
+            insert.b = two;
+            insert.c = three;
+            insert.d = four;
+            
+            dbm.insertROI(insert, PARKING_LOT_ID);
+
+            rois.push_back(insert);
             
             ROIUtils::getROIMats(rois, videoFrame);
 
             clicks = 0;
         }
+    } else if (event == EVENT_RBUTTONUP) { //Delete Roi
+        int indexToDelete = -1;
+
+        for(int i = 0; i < rois.size(); i++) {
+            roi curr = rois[i];
+            vector<Point> contour;
+            contour.push_back(curr.a);
+            contour.push_back(curr.b);
+            contour.push_back(curr.c);
+            contour.push_back(curr.d);
+            if(pointPolygonTest(contour, Point(x,y), true) >= 0) {
+                indexToDelete = i;
+                break;
+            }
+        }
+        
+        if(indexToDelete != -1) {
+            roi roiToDelete = rois[indexToDelete];
+            dbm.deleteROI(roiToDelete.id);
+            rois.erase(rois.begin() + indexToDelete);
+        }
     }
 }
 
 int main(int argc, const char * argv[]) {
-
+    //Get ROIs from the DB.
+    rois = dbm.getROIs();
+    int timeSinceLastLog = 0;
+    
     //Set up video capture
     VideoCapture cap;
     cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
     cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-    cap.open(1);
+    cap.open(0);
     
     //Create window with mouse callback
     namedWindow("window",1);
@@ -99,6 +134,14 @@ int main(int argc, const char * argv[]) {
             cvtColor( videoFrame, videoFrame, CV_GRAY2RGB );
             videoFrame += roisOverlay;
             
+            //Log occupancy information into the DB every 10 mins? Rarely would someone be in a parking spot for < 10 mins?
+            timeSinceLastLog += FRAME_DELAY;
+            
+            if(timeSinceLastLog >= LOG_TIME) {
+                timeSinceLastLog = 0;
+                dbm.logOccupancy(CAMERA_ID, rois);
+            }
+            
             imshow("window", videoFrame);
             
         } catch (Exception& e) {
@@ -106,7 +149,7 @@ int main(int argc, const char * argv[]) {
             std::cout << "Exception caught : imshow:\n" << err_msg << std::endl;
         }
         
-        waitKey(1000);
+        waitKey(FRAME_DELAY);
     }
     
     return 0;
